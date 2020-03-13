9a49ad85d0f0755ea02f47ca901214624178f446
Kayige <52558343+Kayige@users.noreply.github.com>
2020-03-13 16:31:10 +0800

    Update Constants.py
    Fixed pathing issues

--- Constants.py
+++ Constants.py
@@ -1,6 +1,9 @@
 import os
 import pygame
 
+current_path = os.path.dirname(__file__) # Where your .py file is located
+image_path = os.path.join(current_path, 'images') # The resource folder path
+
 pix_sz = 32
 font_sz = pix_sz * 2 / 3
 win_w = 16 * pix_sz
@@ -23,21 +26,18 @@ BLUE = (64, 64, 255)
 LIGHT_BROWN = (255, 255, 0)
 BROWN = (128, 128, 0)
 
-# Images
-BG = "images/background.jpg"
-CHEST = "images/Chest.png"
-TANK = "images/Tank.png"
-WARRIOR = "images/Warrior.png"
-MAGE = "images/Mage.png"
-HEALER = "images/Healer.png"
-WOLF = "images/Wolf.png"
-GHOUL = "images/Ghoul.png"
-GOBLIN = "images/Goblin.png"
-SKELETON = "images/Skeleton.png"
-OGRE = "images/Ogre.png"
-GARGOYLE = "images/Gargoyle.png"
-BOSS = "images/Skeleton King.png"
-PLAYER = "images/Tank_Player.png"
-
-
-
+# src
+BG = os.path.join(image_path, "background.png")
+CHEST = os.path.join(image_path, "Chest.png")
+TANK = os.path.join(image_path, "Tank.png")
+WARRIOR = os.path.join(image_path, "Warrior.png")
+MAGE = os.path.join(image_path, "Mage.png")
+HEALER = os.path.join(image_path, "Healer.png")
+WOLF = os.path.join(image_path, "Wolf.png")
+GHOUL = os.path.join(image_path, "Ghoul.png")
+GOBLIN = os.path.join(image_path, "Goblin.png")
+SKELETON = os.path.join(image_path, "Skeleton.png")
+OGRE = os.path.join(image_path, "Ogre.png")
+GARGOYLE = os.path.join(image_path, "Gargoyle.png")
+BOSS = os.path.join(image_path, "Skeleton_King.png")
+PLAYER = os.path.join(image_path, "Tank_Player.png")
